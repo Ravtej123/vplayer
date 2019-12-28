d41c5d4693ac075073c41b4924054f9de37aa90c
Ravtej <ravtejsingh101@gmail.com>
2019-12-28 22:09:56 +0530

    init commit

--- app/src/main/java/com/example/videoplayer/Main2Activity.java
+++ app/src/main/java/com/example/videoplayer/Main2Activity.java
@@ -4,265 +4,185 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.OpenableColumns;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
-import java.io.File;
+import java.util.Objects;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int PICK_VIDEO_REQUEST = 234;
-    private ImageView imageView;
-    private Button buttonChoose, buttonUpload;
+public class Main2Activity extends AppCompatActivity  {
 
-    private String TAG = getClass().getName();
+    Button selectFile,upload;
+    TextView notification;
+    Uri videoUri;
+    Button fetch;
+    Button vp;
 
-    //private Uri filePath;
-    private ArrayList<MediaFile> mediaFileList;
-
-    private StorageReference storageReference;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
+    //ArrayList<String> urls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main2);
 
-        storageReference = FirebaseStorage.getInstance().getReference();
+        fetch=findViewById(R.id.fetchFiles);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        buttonChoose = (Button) findViewById(R.id.buttonChoose);
-        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Main2Activity.this,MyRecyclerViewActivity.class));
+            }
+        });
 
-        buttonChoose.setOnClickListener(this);
-        buttonUpload.setOnClickListener(this);
+        storage = FirebaseStorage.getInstance();
+        database = FirebaseDatabase.getInstance();
 
-        mediaFileList = new ArrayList<>();
+        selectFile = findViewById(R.id.selectFile);
+        upload = findViewById(R.id.upload);
+        notification = findViewById(R.id.notification);
+        vp = findViewById(R.id.vp);
+
+        selectFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                {
+                    selectVideo();
+
+                }
+                else{
+                    ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 9);
+                }
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(videoUri!=null)
+                uploadFile(videoUri);
+                else
+                    Toast.makeText(Main2Activity.this,"select a file", Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        vp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abc = new Intent(Main2Activity.this,VideoActivity.class);
+                startActivity(abc);
+            }
+        });
     }
 
-    private void showFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("video/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Video"), PICK_VIDEO_REQUEST);
-    }
+    private void uploadFile(Uri videoUri){
 
-    private void uploadFile() {
-/*
-        if (filePath != null) {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.setProgress(0);
+        progressDialog.show();
 
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading....");
-            progressDialog.show();
+        final String fileName = System.currentTimeMillis()+".mp4";
 
+        final String fileName1 = System.currentTimeMillis()+"";
+        StorageReference storageReference = storage.getReference();
+        storageReference.child("Uploads").child(fileName).putFile(videoUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
 
-            StorageReference riversRef = storageReference.child("videos/video1.mp4");
+                        String url= (taskSnapshot.getMetadata()).getReference().getDownloadUrl().toString();
+                        DatabaseReference reference = database.getReference();
 
-            riversRef.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
-
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception exception) {
-                            progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
-
-
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                            progressDialog.setMessage(((int) progress) + "% Uploaded....");
-                        }
-                    });
-
-
-        } else {
-            //display error toast
-        }*/
-
-
-        if (mediaFileList != null && !mediaFileList.isEmpty()) {
-
-            final ProgressDialog progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Uploading....");
-            progressDialog.show();
-
-
-            StorageReference riversRef = storageReference.child("videos");
-            for (MediaFile mediaFile : mediaFileList) {
-                riversRef.child(mediaFile.getFileName()).putFile(mediaFile.getFileUri())
-                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        reference.child(fileName1).setValue(url).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
-                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                progressDialog.dismiss();
-                                Toast.makeText(getApplicationContext(), "File Uploaded", Toast.LENGTH_LONG).show();
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Main2Activity.this,"File successfully uploaded",Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    Toast.makeText(Main2Activity.this,"File Not successfully uploaded",Toast.LENGTH_SHORT).show();
 
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                progressDialog.dismiss();
-                                Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
-
-
-                            }
-                        })
-                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                                double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
-                                progressDialog.setMessage(((int) progress) + "% Uploaded....");
+                                }
                             }
                         });
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                Toast.makeText(Main2Activity.this,"File Not successfully uploaded",Toast.LENGTH_SHORT).show();
             }
-        } else {
-            //display error toast
-            Toast.makeText(this, "Failed to upload files", Toast.LENGTH_SHORT).show();
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+
+                int currentProgress =(int)(100*taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                progressDialog.setProgress(currentProgress);
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if(requestCode==9 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+        {
+            selectVideo();
         }
+        else
+            Toast.makeText(Main2Activity.this,"Please Provide Permission..",Toast.LENGTH_SHORT).show();
+    }
+
+    private void selectVideo(){
+
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent,86);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_VIDEO_REQUEST && resultCode == RESULT_OK) {
-            /*filePath = data.getData();
-
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-            if (mediaFileList == null)
-                mediaFileList = new ArrayList<>();
-            else
-                mediaFileList.clear();
-
-            Log.e(getClass().getName(), "Request Code matched");
-            if (data != null) {
-                Log.e(getClass().getName(), "Data is not null");
-
-                // Checking for selection multiple files or single.
-                if (data.getClipData() != null) {
-                    Log.e(getClass().getName(), "Data has clipData");
-
-                    // Getting the length of data and logging up the logs using index
-                    for (int index = 0; index < data.getClipData().getItemCount(); index++) {
-
-                        // Getting the URIs of the selected files and logging them into logcat at debug level
-                        Uri uri = data.getClipData().getItemAt(index).getUri();
-                        Log.e("filesUri [" + uri + "] : ", String.valueOf(uri));
-
-                        mediaFileList.add(new MediaFile(getFileName(this, uri), uri));
-                    }
-                } else {
-                    Log.e(getClass().getName(), "Data has single file");
-
-                    // Getting the URI of the selected file and logging into logcat at debug level
-                    Uri uri = data.getData();
-                    Log.e("fileUri: ", String.valueOf(uri));
-
-                    mediaFileList.add(new MediaFile(getFileName(this, uri), uri));
-                }
-
-            } else {
-                Log.e(getClass().getName(), "Error getting Pick Code");
-            }
+        if (requestCode == 86 && resultCode == RESULT_OK && data != null) {
+            videoUri = data.getData();
+            notification.setText("A file is selected : " + data.getData().getLastPathSegment());
+        } else {
+            Toast.makeText(Main2Activity.this, "Please select a file", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        if (view == buttonChoose) {
-            if (isStoragePermissionGranted())
-                showFileChooser();
-
-        } else if (view == buttonUpload) {
-
-            uploadFile();
-
-        }
-    }
-
-    public boolean isStoragePermissionGranted() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Log.v(TAG, "Permission is granted");
-                return true;
-            } else {
-
-                Log.v(TAG, "Permission is revoked");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-                return false;
-            }
-        } else { //permission is automatically granted on sdk<23 upon installation
-            Log.v(TAG, "Permission is granted");
-            return true;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            Log.v(getClass().getName(), "Permission: " + permissions[0] + "was " + grantResults[0]);
-            //resume tasks needing this permission
-        }
-    }
-
-    private String getFileName(Context context, Uri uri) {
-        String displayName = "";
-        if (uri != null) {
-            String uriString = uri.toString();
-            if (uriString.startsWith("content://")) {
-                try (Cursor cursor = context.getContentResolver()
-                        .query(uri, null, null, null, null)) {
-                    //Log.e(javaClass.name, "CursorInfo: $cursor")
-                    if (cursor != null && cursor.moveToFirst()) {
-                        displayName =
-                                cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                    }
-                }
-            } else if (uriString.startsWith("file://")) {
-                displayName = new File(uriString).getName();
-            }
-        }
-        return displayName;
     }
 }
---
+++ app/src/main/java/com/example/videoplayer/MyAdapter.java
@@ -0,0 +1,78 @@
+package com.example.videoplayer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    RecyclerView recyclerView;
+    Context context;
+    ArrayList<String> items;
+    ArrayList<String> urls;
+
+    public void update(String name, String url)
+    {
+        items.add(name);
+        urls.add(url);
+        notifyDataSetChanged();
+    }
+
+    public MyAdapter(RecyclerView recyclerView, Context context, ArrayList<String> items,ArrayList<String>  urls) {
+        this.recyclerView = recyclerView;
+        this.context = context;
+        this.items = items;
+        this.urls = urls;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.nameOfFile.setText(items.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView nameOfFile;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameOfFile=itemView.findViewById(R.id.nameOfFile);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("IntentReset")
+                @Override
+                public void onClick(View view) {
+
+                    int position = recyclerView.getChildLayoutPosition(view);
+                    Intent intent = new Intent();
+                    intent.setType(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(urls.get(position)));
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+}
